45ce5615f20cdc3992d9cefc70b51915ba3742f8
Delaine Wendling <delaine.wendling@gmail.com>
2016-10-22 22:54:54 -0500

    Created React Boilerplate

---
+++ app/components/Main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div className="main-container">
+         <h1> Hello World! </h1>
+      </div>
+    )
+  }
+});
+
+module.exports = Main;
